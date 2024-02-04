92b219c9ff1e900c10a657aacbb38bcd13769d26
陳翰毅 <tp6c04u4456@yahoo.com.tw>
2024-02-04 20:59:03 +0800

    修改generateToken中的expiresIn參數

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,8 +2,9 @@ import createHttpError from 'http-errors';
 import jsonWebToken, { type JwtPayload } from 'jsonwebtoken';
 
 export const generateToken = (payload: { userId?: string }) => {
+    console.log('Expires In:', process.env.JWT_EXPIRES_DAY); 
     return jsonWebToken.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_DAY
+        expiresIn: "7d"
     });
 };
 
